33ca60054b3a60231e5bb92922f1a2ece6927154
Nicholas Lim <18374483+niclim@users.noreply.github.com>
2018-05-06 17:16:06 -0700

    added mutations and subscriptions

--- src/index.js
+++ src/index.js
@@ -7,17 +7,55 @@ import { ApolloProvider } from 'react-apollo'
 import { ApolloClient } from 'apollo-client'
 import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
+import { BrowserRouter } from 'react-router-dom'
+import { AUTH_TOKEN } from './constants'
+import { ApolloLink, split } from 'apollo-client-preset'
+import { WebSocketLink } from 'apollo-link-ws'
+import { getMainDefinition } from 'apollo-utilities'
 
 const httpLink = new HttpLink({ uri: 'http://localhost:4000' })
+const middlewareAuthLink = new ApolloLink((operation, forward) => {
+  const token = window.localStorage.getItem(AUTH_TOKEN)
+  const authorizationHeader = token ? `Bearer ${token}` : null
+  operation.setContext({
+    headers: {
+      authorization: authorizationHeader
+    }
+  })
+  return forward(operation)
+})
+
+const httpLinkWithAuthToken = middlewareAuthLink.concat(httpLink)
+
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      authToken: window.localStorage.getItem(AUTH_TOKEN)
+    }
+  }
+})
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query)
+    return kind === 'OperationDefinition' && operation === 'subscription'
+  },
+  wsLink,
+  httpLinkWithAuthToken
+)
 
 const client = new ApolloClient({
-  link: httpLink,
+  link,
   cache: new InMemoryCache()
 })
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>,
   document.getElementById('root'))
 registerServiceWorker()
